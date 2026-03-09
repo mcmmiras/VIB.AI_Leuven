2138f30b1ceca7638c8bd89f05d214167202ce8e
mari <cmmcri@ibmb.csic.es>
2026-03-09 21:53:13 +0100

    folseek

--- saprot_models/my_model_predictfoldseek.py
+++ saprot_models/my_model_predictfoldseek.py
@@ -310,9 +310,9 @@ def train_mlp(model, train_loader, test_loader, class_weights, lr=1e-3, epochs=2
     return model, best_preds, best_labels
 
 def plot_confusion(preds, labels, tag, classes, title="Confusion Matrix"):
-    cm = confusion_matrix(labels, preds, normalize=all)
+    cm = confusion_matrix(labels, preds, normalize="all")
     plt.figure(figsize=(12,10))
-    sns.heatmap(cm, annot=True, fmt="d", cmap="Blues",
+    sns.heatmap(cm, annot=True, fmt=".2f", cmap="Blues",
                 xticklabels=classes, yticklabels=classes)
     plt.xlabel("Predicted")
     plt.ylabel("True")
